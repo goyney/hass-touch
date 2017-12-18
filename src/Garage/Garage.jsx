22f37daf0334e3caf6c0934eda54923faa48a97a
Michael Irigoyen <goyney@gmail.com>
2017-12-17 21:04:29 -0500

    Fix background image on initial load

--- src/Garage/Garage.jsx
+++ src/Garage/Garage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 
+// 12 second traversal time
+
 export default class Garage extends React.Component {
   render() {
     return (
