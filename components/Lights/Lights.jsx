f478ffa161feb40b2d4b3e31db846093ef326980
Michael Irigoyen <goyney@gmail.com>
2017-12-29 15:19:51 -0500

    Light work

--- components/Lights/Lights.jsx
+++ components/Lights/Lights.jsx
@@ -47,15 +47,19 @@ export default class Lights extends React.Component {
         const switchId = switcher.split('.', 2)[1];
         return Object.assign(output, Object.keys(switches).reduce((allSwitches, type) => {
           if (switchId in switches[type]) {
-            // console.log(switches[type][switchId]);
             allSwitches[switches[type][switchId].entity_id] = {
               name: switches[type][switchId].attributes.short_name || switches[type][switchId].attributes.friendly_name,
               state: switches[type][switchId].state,
               icon: switches[type][switchId].attributes.icon,
-              speed: switches[type][switchId].attributes.speed || undefined,
-              speedList: switches[type][switchId].attributes.speed_list || undefined,
               type
             };
+            if (type === 'fan') {
+              allSwitches[switches[type][switchId].entity_id].speed = switches[type][switchId].attributes.speed;
+              allSwitches[switches[type][switchId].entity_id].speedList = switches[type][switchId].attributes.speed_list;
+            }
+            if (type === 'light') {
+              allSwitches[switches[type][switchId].entity_id].brightness = Math.ceil(switches[type][switchId].attributes.brightness / 2.56);
+            }
           }
           return allSwitches;
         }, {}));
@@ -64,6 +68,7 @@ export default class Lights extends React.Component {
       groupsOutput[groups[group].entity_id] = {
         name: groups[group].attributes.friendly_name,
         state: groups[group].state,
+        controls: groups[group].attributes.control,
         switches: sortBy(groupSwitches, 'name')
       };
       return groupsOutput;
@@ -94,7 +99,8 @@ export default class Lights extends React.Component {
               })}
             >
               <i className={`mdi ${switcher.icon.replace(':', '-')}`}>
-                {switcher.type === 'fan' && switcher.state === 'on' && <span>{switcher.speed}</span>}
+                {switcher.type === 'fan' && switcher.state === 'on' && switcher.speed !== 'off' && <span>{switcher.speed}</span>}
+                {switcher.type === 'light' && switcher.state === 'on' && !isNaN(switcher.brightness) && <span>{switcher.brightness}%</span>}
               </i>
               {switcher.name}
             </div>
@@ -109,7 +115,7 @@ export default class Lights extends React.Component {
         >
           <h2>
             {group.name}
-            {switches.length > 1 && <Checkbox
+            {switches.length > 1 && group.controls !== 'hidden' && <Checkbox
               toggle
               checked={group.state === 'on'}
               onChange={this._toggle(groupId)}
@@ -129,7 +135,7 @@ export default class Lights extends React.Component {
     const { connection } = this.props;
     const currentGroups = this.state.groups;
 
-    if (groupEntityId !== 'group.master_bedroom') {
+    // if (groupEntityId !== 'group.master_bedroom') {
       if (switchEntityId) {
         currentGroups[groupEntityId].switches[switchEntityId].state = currentGroups[groupEntityId].switches[switchEntityId].state === 'on' ? 'off' : 'on';
         if (currentGroups[groupEntityId].switches[switchEntityId].state === 'on') {
@@ -153,9 +159,9 @@ export default class Lights extends React.Component {
         groups: currentGroups,
         lastUpdated: Date.now()
       });
-    } else {
-      console.log('Relationship Insurance Activated');
-    }
+    // } else {
+    //   console.log('Relationship Insurance Activated');
+    // }
   }
 
   _triggerSettings = () => {
